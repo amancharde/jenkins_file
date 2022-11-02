663360fb0fd39c9853998b2d0ee4263c5445d074
amancharde <110483619+amancharde@users.noreply.github.com>
2022-11-02 12:45:11 +0530

    Update cfile.py

--- cfile.py
+++ cfile.py
@@ -1,17 +1,4 @@
-pipeline
-{
-  agent any
-  stages
-  {
-    stage('build')
-    {
-      steps
-      {
-        i=4
-        j=5
-        k=i+j
-        print("hello",k)
-      }
-    }
-  }
-}
+i=5
+j=7
+k=i+j
+print("the answer is:",k)
